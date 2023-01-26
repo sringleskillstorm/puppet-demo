ea1bd2c036a311e5f8bf7f35e1612b3010468caa
sringleskillstorm <121134907+sringleskillstorm@users.noreply.github.com>
2023-01-26 12:53:56 -0600

    Update ntp_example.pp

--- ntp_example.pp
+++ ntp_example.pp
@@ -8,6 +8,6 @@ node default {
   file { 'webbpage':
              path => '/var/www/html/index.html',
              ensure => 'file',
-             source => 'https://github.com/sringleskillstorm/puppet-demo/blob/b25b16165c294ca6a2d8c727667f25be5f310ec7/ntp_example.pp',
+             source => 'https://raw.githubusercontent.com/sringleskillstorm/puppet-demo/main/serverDate.html',
         }
 }
