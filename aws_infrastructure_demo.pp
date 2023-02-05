55da61acf6a23743c4afd8185e1ef4ed94ff0225
sringleskillstorm <121134907+sringleskillstorm@users.noreply.github.com>
2023-02-05 12:45:57 -0600

    Update aws_infrastructure_demo.pp

--- aws_infrastructure_demo.pp
+++ aws_infrastructure_demo.pp
@@ -1,11 +1,9 @@
-node default {
-    ec2_instance { 'agent007':
-        ensure => running,
-        region => 'us-east-1',
-        image_id => 'ami-0aa7d40eeae50c9a9',
-        instance_type => 't3.nano',
-        key_name => 'sringle',
-        subnet => 'puppet-demo-public-subnet',
-        security_groups => ['puppet-demo-sg'],
-    }
+ec2_instance { 'agent007':
+    ensure => running,
+    region => 'us-east-1',
+    image_id => 'ami-0aa7d40eeae50c9a9',
+    instance_type => 't2.nano',
+    key_name => 'sringle',
+    subnet => 'puppet-demo-public-subnet',
+    security_groups => ['puppet-demo-sg'],
 }
