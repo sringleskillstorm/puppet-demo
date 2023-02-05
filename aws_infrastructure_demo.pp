cb389d3c9f667d00b03b015c2790d48376151e4f
sringleskillstorm <121134907+sringleskillstorm@users.noreply.github.com>
2023-02-05 12:51:53 -0600

    Update aws_infrastructure_demo.pp

--- aws_infrastructure_demo.pp
+++ aws_infrastructure_demo.pp
@@ -2,8 +2,8 @@ ec2_instance { 'agent007':
     ensure => running,
     region => 'us-east-1',
     image_id => 'ami-0aa7d40eeae50c9a9',
-    instance_type => 't2.nano',
-    key_name => 'YOUR KEY NAME HERE',
+    instance_type => 't3.nano',
+    key_name => 'sringle',
     subnet => 'puppet-demo-public-subnet',
     security_groups => ['puppet-demo-sg'],
 }
